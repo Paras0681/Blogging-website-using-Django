08e0f4670845d746be4debaf09b35c054ea8ca01
Paras Kamble <77244089+Paras0681@users.noreply.github.com>
2021-08-30 18:32:55 +0530

    Delete blogs directory

--- blogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-# from . import views
-from . views import AddPostView, HomeView, ArticleDetailView
-urlpatterns = [
-	# path('', views.home, name='home'),
-	path('', HomeView.as_view(), name='home'),
-	path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-	path('addpost/', AddPostView.as_view(), name='addpost'),
-]
